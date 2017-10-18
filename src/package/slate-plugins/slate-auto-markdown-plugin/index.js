c9be01abd4cfdab8c84293bdbca052770ed3e500
Jeff Lai <winfield301@gmail.com>
2017-10-18 11:12:36 +0800

    Fix AutoMarkdownPlugin according to breaking changes

--- src/package/slate-plugins/slate-auto-markdown-plugin/index.js
+++ src/package/slate-plugins/slate-auto-markdown-plugin/index.js
@@ -3,11 +3,11 @@ import { onSpace, onEnter, onBackspace } from './AutoMarkdownEvents'
 
 const AutoMarkdownPlugin = () => ({
   schema: AutoMarkdownSchema,
-  onKeyDown: (event, data, change) => {
-    switch (data.key) {
-      case 'space': return onSpace(event, change)
-      case 'backspace': return onBackspace(event, change)
-      case 'enter': return onEnter(event, change)
+  onKeyDown: (event, change) => {
+    switch (event.key) {
+      case ' ': return onSpace(event, change)
+      case 'Backspace': return onBackspace(event, change)
+      case 'Enter': return onEnter(event, change)
       default: return false
     }
   },
