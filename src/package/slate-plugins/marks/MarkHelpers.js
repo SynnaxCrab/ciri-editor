e100a48eb176598e8a0176e62298a28a81e39a73
Jeff Lai <winfield301@gmail.com>
2019-03-06 15:29:02 +0800

    Update marks plugin using latest slate

--- src/package/slate-plugins/marks/MarkHelpers.js
+++
@@ -1,3 +0,0 @@
-export const hasMark = (type, activeMarks) => activeMarks.some(mark => mark.type === type)
-
-export const toggleMark = (change, type) => change.toggleMark(type)
--- src/package/slate-plugins/marks/index.js
+++ src/package/slate-plugins/marks/index.js
@@ -1,26 +1,58 @@
-import renderMark from './MarkRender'
-import MarkButton from './MarkButton'
-import { toggleMark, hasMark } from './MarkHelpers'
-
+import React from 'react'
 import HotKey from '../slate-hotkey'
 
-const Marks = marks => ({
-  helpers: {
-    hasMark,
-  },
-  changes: {
-    toggleMark,
-  },
-  plugins: [
-    ...marks.map(({key, type}) => (
-      HotKey({
-        key,
-        transform: t => toggleMark(t, type)
-      })
-    )),
-    { renderMark },
-  ],
-})
+import HoveringMenu from './HoveringMenu'
+
+const command = (editor, type) => editor.toggleMark(type)
+
+const renderEditor = (props, editor, next) => {
+  const children = next()
+  return (
+    <>
+      {children}
+      <HoveringMenu editor={editor} />
+    </>
+  )
+}
+
+const renderNode = (props, editor, next) => {
+  switch (props.node.type) {
+    case 'code':
+      return (
+        <pre {...props.attributes}>
+          <code>{props.children}</code>
+        </pre>
+      )
+    default:
+      return next()
+  }
+}
+
+const renderMark = (props, editor, next) => {
+  switch (props.mark.type) {
+    case 'bold':
+      return <strong>{props.children}</strong>
+    case 'code':
+      return <code>{props.children}</code>
+    case 'italic':
+      return <em>{props.children}</em>
+    case 'underlined':
+      return <u>{props.children}</u>
+    default:
+      return next()
+  }
+}
+
+const Marks = marks => [
+  { renderMark },
+  { renderNode },
+  { renderEditor },
+  ...marks.map(({ key, type }) =>
+    HotKey({
+      key,
+      command: editor => command(editor, type),
+    }),
+  ),
+]
 
 export default Marks
-export { MarkButton }
